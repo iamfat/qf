b159a51fd997e0af99da42857bc3e682849c4cca
diguangzhao <1280029266@qq.com>
2018-02-08 17:15:10 +0800

    Q语句排序增加表选择 sort tablename.field_name 所有列表展示内容，都增加点击表头可排序的功能 RQ163734

--- libraries/q/pseudo/sort.php
+++ libraries/q/pseudo/sort.php
@@ -4,7 +4,7 @@ class Q_Pseudo_Sort implements Q_Pseudo {
 
 	static $guid = 0;
 	
-	const SORT_PATTERN = '/([\w\pL_-]+)\s*(DESC|ASC|↓|D|A|↑)?(\s*,\s*)?/';
+	const SORT_PATTERN = '/([\w\pL._-]+)\s*(DESC|ASC|↓|D|A|↑)?(\s*,\s*)?/';
 
 	private $_query;
 
@@ -17,10 +17,13 @@ class Q_Pseudo_Sort implements Q_Pseudo {
 		if (preg_match_all(self::SORT_PATTERN, $selector, $parts, PREG_SET_ORDER)) {
 			$db = $query->db;
 			foreach($parts as $part){
-				$field = $part[1];
+				$field_str = $part[1];
+				$field_arr = explode('.', $field_str);
+				$field = array_pop($field_arr);
+				$table = count($field_arr) ? $query->alias[array_pop($field_arr)] : $query->table;
 				$order = $part[2];
 				$order=preg_match('/^↓|D|DESC$/', $order) ? 'DESC':'ASC';
-				$query->order_by[] = $db->make_ident($query->table, $field).' '.$order;
+				$query->order_by[] = $db->make_ident($table, $field).' '.$order;
 			}
 		}
 	}
