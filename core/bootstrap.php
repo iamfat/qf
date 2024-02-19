5a014e8399fdbfc61aeccfcd078fa70a696df968
Jia Huang <iamfat@gmail.com>
2024-02-19 13:12:06 +0800

    feat: better error output

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,62 +1,95 @@
 <?php
 
-define('CORE_PATH', dirname(__FILE__).'/');
+define('CORE_PATH', dirname(__FILE__) . '/');
 
-require CORE_PATH.'def.php';
-require CORE_PATH.'exception.php';
-require CORE_PATH.'cache.php';
-require CORE_PATH.'core.php';
+require CORE_PATH . 'def.php';
+require CORE_PATH . 'exception.php';
+require CORE_PATH . 'cache.php';
+require CORE_PATH . 'core.php';
 
-final class CGI {
-	static function shutdown() {
+final class CGI
+{
+	static function shutdown()
+	{
 		Event::trigger('system.output');		// 系统显示事件
 		Event::trigger('system.shutdown');		// 系统关闭事件
 		Core::shutdown();						// 内核关闭
 	}
 
-	static function exception($e) {
+	static function exception($e)
+	{
 		$message = $e->getMessage();
 		if ($message) {
 			$file = File::relative_path($e->getFile());
 			$line = $e->getLine();
-			error_log(sprintf("\x1b[31m\x1b[4mEXCEPTION\x1b[0m \x1b[1m%s\x1b[0m", $message));
-			$trace = array_slice($e->getTrace(), 1, 5);
-			foreach ($trace as $n => $t) {
-				error_log(sprintf("    %d) %s%s() in %s on line %d", $n + 1,
-								$t['class'] ? $t['class'].'::':'', 
-								$t['function'],
-								File::relative_path($t['file']),
-								$t['line']));
-
+			error_log(sprintf("EXCEPTION %s (%s:%d)\n", $message, $file, $line));
+			if (defined('DEBUG')) {
+				$trace = array_slice($e->getTrace(), 1, 5);
+				foreach ($trace as $n => $t) {
+					error_log(sprintf(
+						"    %d) %s%s() (%s:%d)",
+						$n + 1,
+						$t['class'] ? $t['class'] . '::' : '',
+						$t['function'],
+						File::relative_path($t['file']),
+						$t['line']
+					));
+				}
 			}
 		}
 
 		if (PHP_SAPI != 'cli') {
-			while(@ob_end_clean());	//清空之前的所有显示
+			while (@ob_end_clean());	//清空之前的所有显示
 			header('HTTP/1.1 500 Internal Server Error');
-		}		
+		}
 	}
 
-	static function error($errno, $errstr, $errfile, $errline, $errcontext) {
-		error_log(sprintf("\x1b[31m\x1b[4mERROR\x1b[0m \x1b[1m%s (%s:%d) \x1b[0m", $errstr, $errfile, $errline));
+	static function error($errno, $errstr, $errfile, $errline)
+	{
+		static $errors = [
+			E_ERROR => "ERROR",
+			E_CORE_ERROR => "CORE_ERROR",
+			E_COMPILE_ERROR => "COMPILE_ERROR",
+			E_USER_ERROR => "USER_ERROR",
+			E_STRICT => "STRICT",
+			E_RECOVERABLE_ERROR => "RECOVERABLE_ERROR",
+		];
+
+		static $others = [
+			E_WARNING => "WARNING",
+			E_PARSE => "PARSE",
+			E_NOTICE => "NOTICE",
+			E_CORE_WARNING => "CORE_WARNING",
+			E_COMPILE_WARNING => "COMPILE_WARNING",
+			E_USER_WARNING => "USER_WARNING",
+			E_USER_NOTICE => "USER_NOTICE",
+			E_STRICT => "STRICT",
+			E_DEPRECATED => "DEPRECATED",
+			E_USER_DEPRECATED => "USER_DEPRECATED",
+		];
+
+		if (isset($errors[$errno])) {
+			error_log(sprintf("%s %s (%s:%d)\n", $errors[$errno], $errstr, $errfile, $errline));
+		} else if (defined('DEBUG') && isset($others[$errno])) {
+			error_log(STDERR, sprintf("%s %s (%s:%d)\n", $others[$errno], $errstr, $errfile, $errline));
+		}
 		// throw new ErrorException($errstr, $errno, 1, $errfile, $errline);
 	}
 
-	static function assertion($file, $line, $code) {
+	static function assertion($file, $line, $code)
+	{
 		throw new ErrorException($code, 0, 1, $file, $line);
 	}
-
 }
 
 register_shutdown_function('CGI::shutdown');
 set_exception_handler('CGI::exception');
 set_error_handler('CGI::error', E_ALL & ~E_NOTICE);
 
-assert_options(ASSERT_ACTIVE, 1);
-assert_options(ASSERT_WARNING, 0);
-assert_options(ASSERT_QUIET_EVAL, 1);
-assert_options(ASSERT_CALLBACK, 'CGI::assertion');
-
+// assert_options(ASSERT_ACTIVE, 1);
+// assert_options(ASSERT_WARNING, 0);
+// assert_options(ASSERT_QUIET_EVAL, 1);
+// assert_options(ASSERT_CALLBACK, 'CGI::assertion');
 
 Core::setup();						// 内核启动
 Core::bind_events();				// 绑定内核事件
@@ -65,4 +98,3 @@ Event::trigger('system.setup');		// 系统初始化事件
 Event::trigger('system.ready');		// 系统就绪事件
 
 Core::dispatch();					// 分派控制器
-
--- core/cli.php
+++ core/cli.php
@@ -1,57 +1,90 @@
 <?php
 
-define('CORE_PATH', dirname(__FILE__).'/');
+define('CORE_PATH', dirname(__FILE__) . '/');
 
-require CORE_PATH.'def.php';
-require CORE_PATH.'exception.php';
-require CORE_PATH.'cache.php';
-require CORE_PATH.'core.php';
+require CORE_PATH . 'def.php';
+require CORE_PATH . 'exception.php';
+require CORE_PATH . 'cache.php';
+require CORE_PATH . 'core.php';
 
-final class CLI {
-	static function shutdown() {
+final class CLI
+{
+	static function shutdown()
+	{
 		Event::trigger('system.output');
 		Event::trigger('system.shutdown');
 		Core::shutdown();
 	}
 
-	static function exception($e) {
+	static function exception($e)
+	{
 		$message = $e->getMessage();
 		$file = File::relative_path($e->getFile());
 		$line = $e->getLine();
-		printf("[exception] \x1b[1m%s\x1b[0m (\x1b[34m%s\x1b[0m:$line)\n", $message, $file, $line);
+		printf("\x1b[31;4mEXCEPTION\x1b[0m %s (\x1b[34m%s\x1b[0m:%d)\n", $message, $file, $line);
 		if (defined('DEBUG')) {
-			$trace = array_slice($e->getTrace(), 1, 3);
+			$trace = array_slice($e->getTrace(), 1, 5);
 			foreach ($trace as $n => $t) {
-				fprintf(STDERR, "%3d. %s%s() in (%s:%d)\n", $n + 1,
-								$t['class'] ? $t['class'].'::':'', 
-								$t['function'],
-								File::relative_path($t['file']),
-								$t['line']);
-
+				fprintf(
+					STDERR,
+					"%3d. %s%s() in (%s:%d)\n",
+					$n + 1,
+					$t['class'] ? $t['class'] . '::' : '',
+					$t['function'],
+					File::relative_path($t['file']),
+					$t['line']
+				);
 			}
 			fprintf(STDERR, "\n");
 		}
 	}
 
-	static function error($errno , $errstr, $errfile, $errline, $errcontext) {
-		error_log(sprintf("\x1b[31m\x1b[4mERROR\x1b[0m \x1b[1m%s (%s:%d) \x1b[0m", $errstr, $errfile, $errline));
+	static function error($errno, $errstr, $errfile, $errline)
+	{
+		static $errors = [
+			E_ERROR => "\x1b[31;4mERROR\x1b[0m",
+			E_CORE_ERROR => "\x1b[31;4mCORE_ERROR\x1b[0m",
+			E_COMPILE_ERROR => "\x1b[31;4mCOMPILE_ERROR\x1b[0m",
+			E_USER_ERROR => "\x1b[31;4mUSER_ERROR\x1b[0m",
+			E_STRICT => "\x1b[31;4mSTRICT\x1b[0m",
+			E_RECOVERABLE_ERROR => "\x1b[31;4mRECOVERABLE_ERROR\x1b[0m",
+		];
+
+		static $others = [
+			E_WARNING => "\x1b[33mWARNING\x1b[0m",
+			E_PARSE => "\x1b[31mPARSE\x1b[0m",
+			E_NOTICE => "\x1b[32mNOTICE\x1b[0m",
+			E_CORE_WARNING => "\x1b[33mCORE_WARNING\x1b[0m",
+			E_COMPILE_WARNING => "\x1b[33mCOMPILE_WARNING\x1b[0m",
+			E_USER_WARNING => "\x1b[33mUSER_WARNING\x1b[0m",
+			E_USER_NOTICE => "\x1b[32mUSER_NOTICE\x1b[0m",
+			E_STRICT => "\x1b[31;4mSTRICT\x1b[0m",
+			E_DEPRECATED => "\x1b[30;1mDEPRECATED\x1b[0m",
+			E_USER_DEPRECATED => "\x1b[30;1mUSER_DEPRECATED\x1b[0m",
+		];
+
+		if (isset($errors[$errno])) {
+			fprintf(STDERR, sprintf("%s %s (\x1b[34m%s\x1b[0m:%d)\n", $errors[$errno], $errstr, $errfile, $errline));
+		} else if (defined('DEBUG') && isset($others[$errno])) {
+			fprintf(STDERR, sprintf("%s %s (\x1b[34m%s\x1b[0m:%d)\n", $others[$errno], $errstr, $errfile, $errline));
+		}
 		// throw new \ErrorException($errstr, $errno, 1, $errfile, $errline);
 	}
 
-	static function assertion($file, $line, $code) {
+	static function assertion($file, $line, $code)
+	{
 		throw new \ErrorException($code, 0, 1, $file, $line);
 	}
-
 }
 
-register_shutdown_function ('CLI::shutdown');
+register_shutdown_function('CLI::shutdown');
 set_exception_handler('CLI::exception');
 set_error_handler('CLI::error', E_ALL & ~E_NOTICE);
 
-assert_options(ASSERT_ACTIVE, 1);
-assert_options(ASSERT_WARNING, 0);
-assert_options(ASSERT_QUIET_EVAL, 1);
-assert_options(ASSERT_CALLBACK, 'CGI::assertion');
+// assert_options(ASSERT_ACTIVE, 1);
+// assert_options(ASSERT_WARNING, 0);
+// assert_options(ASSERT_QUIET_EVAL, 1);
+// assert_options(ASSERT_CALLBACK, 'CGI::assertion');
 
 
 Core::setup();
